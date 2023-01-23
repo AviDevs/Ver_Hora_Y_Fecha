6a9dde5a206fddbc545d3974d0c2b9bb75e7ec2e
AviDev <105164770+AviDevs@users.noreply.github.com>
2023-01-22 20:39:05 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace TutorialsYoutube
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblhora = new System.Windows.Forms.Label();
+            this.lblfecha = new System.Windows.Forms.Label();
+            this.horayfecha = new System.Windows.Forms.Timer(this.components);
+            this.btnAcercaDe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblhora
+            // 
+            this.lblhora.AutoSize = true;
+            this.lblhora.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblhora.Location = new System.Drawing.Point(192, 156);
+            this.lblhora.Name = "lblhora";
+            this.lblhora.Size = new System.Drawing.Size(85, 40);
+            this.lblhora.TabIndex = 0;
+            this.lblhora.Text = "Hora";
+            // 
+            // lblfecha
+            // 
+            this.lblfecha.AutoSize = true;
+            this.lblfecha.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblfecha.Location = new System.Drawing.Point(519, 156);
+            this.lblfecha.Name = "lblfecha";
+            this.lblfecha.Size = new System.Drawing.Size(95, 40);
+            this.lblfecha.TabIndex = 1;
+            this.lblfecha.Text = "Fecha";
+            // 
+            // horayfecha
+            // 
+            this.horayfecha.Enabled = true;
+            this.horayfecha.Tick += new System.EventHandler(this.horayfecha_Tick);
+            // 
+            // btnAcercaDe
+            // 
+            this.btnAcercaDe.Location = new System.Drawing.Point(353, -1);
+            this.btnAcercaDe.Name = "btnAcercaDe";
+            this.btnAcercaDe.Size = new System.Drawing.Size(75, 23);
+            this.btnAcercaDe.TabIndex = 2;
+            this.btnAcercaDe.Text = "Acerca De";
+            this.btnAcercaDe.UseVisualStyleBackColor = true;
+            this.btnAcercaDe.Click += new System.EventHandler(this.btnAcercaDe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TutorialsYoutube.Properties.Resources._13686;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAcercaDe);
+            this.Controls.Add(this.lblfecha);
+            this.Controls.Add(this.lblhora);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Fecha Y Hora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblhora;
+        private System.Windows.Forms.Label lblfecha;
+        private System.Windows.Forms.Timer horayfecha;
+        private System.Windows.Forms.Button btnAcercaDe;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TutorialsYoutube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void horayfecha_Tick(object sender, EventArgs e)
+        {
+            lblfecha.Text = DateTime.Now.ToString("hh:mm:ss");
+            lblhora.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void btnAcercaDe_Click(object sender, EventArgs e)
+        {
+            string descripcion = "Mi programa es una aplicación Para ver La hora y la Fecha Facil de Programar . \n\n Creado por: AviDevs \n Version: 1.0 \n Actualizacion #1 Sigueme En Youtube \n ";
+            MessageBox.Show(descripcion, "Acerca de", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
